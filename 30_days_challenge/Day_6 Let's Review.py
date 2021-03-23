af63d9a3e56b6da17bbebf79f1eb0b1474224923
Suresh <78913701+VskShinobi@users.noreply.github.com>
2021-03-23 19:58:36 +0530

    Add files via upload

---
+++ 30_days_challenge/Day_6 Let's Review.py
@@ -0,0 +1,8 @@
+# word = input()
+# for i in range(len(word)):
+#     print(word[::2])
+for i in range(int(input())):
+    word = input()
+    print(word[::2], end=" ")
+    # print(' ')
+    print(word[1::2])
